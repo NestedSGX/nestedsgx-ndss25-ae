f282df383bf7a5e992d35b0d9f56b61cbdb33656
Unik-lif <songlinke18@mails.ucas.ac.cn>
2024-10-03 00:01:18 +0800

    update wolfssl benchmarks

--- benchmarks/microbenchmarks/wolfssl/prepare.sh
+++ benchmarks/microbenchmarks/wolfssl/prepare.sh
@@ -1,3 +1,16 @@
+echo "download wolfssl"
 git clone https://github.com/wolfSSL/wolfssl-examples.git
+cd wolfssl-examples && git am ../diff.patch
 wget https://github.com/wolfSSL/wolfssl/archive/refs/tags/v5.7.0-stable.tar.gz
 tar -zxvf v5.7.0-stable.tar.gz
+
+echo "build the environment: options.h"
+cd  wolfssl-5.7.0-stable 
+./autogen.sh && ./configure && ./config.status
+cd IDE/LINUX_SGX
+
+echo "build the static library"
+./build.sh
+
+cd ../../wolfssl-examples/SGX_Linux
+./build.sh ../../wolfssl-5.7.0-stable/
